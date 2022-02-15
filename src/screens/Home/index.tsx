4e9144362fb7b5c553959375fb18e5db22d48cda
Luis Carlos <luiscarlosnors@gmail.com>
2022-02-14 22:22:20 -0300

    chore: add styled components

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Text } from 'react-native';
-import { Container } from './styles';
+import { Container, Title } from './styles';
 
 export function Home() {
     return (
         <Container>
-            <Text></Text>
+            <Title>HOME</Title>
         </Container>
     );
 }
